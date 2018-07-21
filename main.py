ca3bf2be2b9abaccacfc09c04feb8be2f46e847f
Kevin Frantz <mail@kevin-frantz.de>
2018-07-21 16:44:50 +0200

    Some text formatation

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from gender.Female import Female
 """
     Overview over the articles:
 """
-print ('artìculo:')
+print ('artìculo:\n')
 article = Article()
 def articleSpecific(article:Article,kind:str):
-    print('artículo {0}: {1}'.format(kind,article.getSingular()))
-    print('artículo {0}: {1}'.format(kind,article.getPlural()))
+    print('artículo {0} singular:\t{1}'.format(kind,article.getSingular()))
+    print('artículo {0} plural:\t{1}'.format(kind,article.getPlural()))
 def articleOverview(article:Article,gender:Gender):
     article.setGender(gender)
     print ('{}:'.format(gender.getName()))
     articleSpecific(article.getDefinite(),'específico')
     articleSpecific(article.getUndefinite(),'indefinido')
+    print()
 articleOverview(article,Female())
 articleOverview(article,Male())
