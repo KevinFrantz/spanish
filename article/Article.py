a751bc5559a015a628f72515a8caf8bf3a2d2629
Kevin Frantz <mail@kevin-frantz.de>
2018-07-21 17:41:51 +0200

    Cleaned up article ruleset

--- article/Article.py
+++ article/Article.py
@@ -5,19 +5,13 @@ class Article(object):
     """
     This class represents the ruleset of an article
     """
-    def __init__(self):
-        self.definite = None
-        self.undefinite = None
-        self.__gender = None
-    def __setRuleset(self):
-        self.definite = Definite(self)
-        self.undefinite = Undefinite(self)
-    def setGender(self,gender):
+    def __init__(self,gender):
         self.__gender = gender
-        self.__setRuleset()
+        self.__definite = Definite(self)
+        self.__undefinite = Undefinite(self)
     def getGender(self):
         return self.__gender
     def getDefinite(self)->str:
-        return self.definite
+        return self.__definite
     def getUndefinite(self)->str:
-        return self.undefinite
+        return self.__undefinite
--- main.py
+++ main.py
@@ -6,15 +6,13 @@ from gender.Female import Female
     Overview over the articles:
 """
 print ('artìculo:\n')
-article = Article()
 def articleSpecific(article:Article,kind:str):
     print('artículo {0} singular:\t{1}'.format(kind,article.getSingular()))
     print('artículo {0} plural:\t{1}'.format(kind,article.getPlural()))
-def articleOverview(article:Article,gender:Gender):
-    article.setGender(gender)
-    print ('{}:'.format(gender.getName()))
+def articleOverview(article:Article):
+    print ('{}:'.format(article.getGender().getName()))
     articleSpecific(article.getDefinite(),'específico')
     articleSpecific(article.getUndefinite(),'indefinido')
     print()
-articleOverview(article,Female())
-articleOverview(article,Male())
+articleOverview(Article(Female()))
+articleOverview(Article(Male()))
